b799de908e11a565e56087ef9b4f7318bdce5a50
Sayantan Nandi <sayantannandi13@gmail.com>
2025-06-07 00:18:55 +0530

    auth setup done

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 config();
 import cors from "cors";
 import { PrismaClient } from "../src/generated/prisma";
+import bcrpyt from "bcrypt";
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,69 @@ const FORM_FILLER_API_URL = process.env.FORM_FILLER_API_URL;
 const FORM_ANALYZER_API_URL = process.env.FORM_ANALYZER_API_URL;
 
 const prisma = new PrismaClient();
+
+const hashSalt = 10;
+
+// @ts-ignore
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).send("Username and password are required");
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    const hashedPassword = await bcrpyt.hash(password, hashSalt);
+
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password : hashedPassword
+      },
+    });
+
+    res.status(201).json({ userId : user.id });
+  }
+  catch (error) {
+    console.error("Error signing up:", error);
+    res.status(500).send("Error signing up");
+  }
+});
+
+// @ts-ignore
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).send("Username and password are required");
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const isPasswordValid = await bcrpyt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).send("Invalid password");
+    }
+
+    res.status(200).json({ userId : user.id });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).send("Error logging in");
+  }
+});
 
 // @ts-ignore
 app.post("/create-form", async (req, res) => {
